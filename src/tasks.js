9a45fd829015b1741740fcf2dcbe19b46984201c
Kaushalye <115895359+kaushalye1234@users.noreply.github.com>
2026-09-19 18:00:43 +0530

    change the tasks

--- src/tasks.js
+++ src/tasks.js
@@ -4,7 +4,6 @@ const tasks = [
   "Design the menu screen",
   "Build the orders API",
   "Add user login",
-  "Add payment integration",
 ];
 
 console.log(`CampusEats has ${tasks.length} open tasks`);
